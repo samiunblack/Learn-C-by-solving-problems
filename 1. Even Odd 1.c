971b6f639d0c3444c834191e7c03816e5da9a438
Samiun Black <samiunblack@gmail.com>
2022-03-14 17:43:17 +0600

    add problem 1

---
+++ 1. Even Odd 1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main () {
+
+    int t, i, n;
+
+    scanf("%d", &t);
+
+    for (i = 1 ; i <= t ; i++ ){
+
+    scanf("%d",&n);
+
+    if( n%2==0){
+
+            printf("even\n");
+
+        }
+
+    else{
+
+            printf("odd\n");
+
+        }
+
+    }
+
+    return 0;
+
+ }
